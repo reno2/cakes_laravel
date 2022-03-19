23b4342498024c13a2d2283936af8ca3f298806f
an_zharskiy <chedia@mail.ru>
2022-03-20 01:50:25 +0300

    fix: sort active

--- resources/views/admin/components/sort.blade.php
+++ resources/views/admin/components/sort.blade.php
@@ -1,5 +1,9 @@
 @php
 $querySting = '/?';
+function checkIsActive($name){
+    if(key_exists($name, $_GET)) return true;
+    return false;
+}
 if(isset($_GET['page']) && !empty($_GET['page']) ):
 $querySting = "/?page={$_GET['page']}";
 endif;
@@ -8,10 +12,10 @@ endif;
 <div class="sorts info-cards">
         <div class="info-cards__line sorts__items">
             <span class="sorts__item">Сортирорвка:</span>
-            <a class="sorts__item btn btn-small btn-grey" href="{{url()->current()}}{{$querySting}}moderate=desc">Модерации</a>
-            <a class="sorts__item btn btn-small btn-grey" href="{{url()->current()}}{{$querySting}}published=desc">Опубликованно</a>
-            <a class="sorts__item btn btn-small btn-grey" href="{{url()->current()}}{{$querySting}}on_front=desc">На главной</a>
-            <a class="sorts__item btn btn-small btn-grey" href="{{url()->current()}}{{$querySting}}updated_at=desc">Дате изменения</a>
+            <a class="sorts__item btn btn-small btn-grey @if(checkIsActive('moderate')) btn-secondary @endif" href="{{url()->current()}}{{$querySting}}moderate=desc">Модерации</a>
+            <a class="sorts__item btn btn-small btn-grey @if(checkIsActive('published')) btn-secondary @endif" href="{{url()->current()}}{{$querySting}}published=desc">Опубликованно</a>
+            <a class="sorts__item btn btn-small btn-grey @if(checkIsActive('on_front')) btn-secondary @endif" href="{{url()->current()}}{{$querySting}}on_front=desc">На главной</a>
+            <a class="sorts__item btn btn-small btn-grey @if(checkIsActive('updated_at')) btn-secondary @endif" href="{{url()->current()}}{{$querySting}}updated_at=desc">Дате изменения</a>
             <a class="sorts__item btn btn-small btn-main" href="{{route('admin.article.index')}}">Сбросить</a>
         </div>
 </div>
