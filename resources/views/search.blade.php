05fda25d95185fec5abd18d5046535d0ad56b362
an_zharskiy <chedia@mail.ru>
2022-05-30 23:49:58 +0300

    phone mask, cache

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -32,14 +32,3 @@
 {{--        {{$ads->links()}}--}}
     </ul>
 @endsection
-{{--Тут подключаем нужные стили и скрипты для шаблонов форм--}}
-@section('page-script')
-    <link href="{{asset('css/libs/jQuery.Brazzers-Carousel.min.css')}}" rel="stylesheet"/>
-    <script src="{{asset('js/libs/jQuery.Brazzers-Carousel.min.js')}}"></script>
-    <script>
-        $(document).ready(function () {
-            $(".ad__desktop").brazzersCarousel();
-        })
-
-    </script>
-@stop
